57ea291e77c397ba8ddb01eff7a41e4d159d7900
Sumit Kumar <your_email@example.com>
2025-12-21 19:09:39 +0530

    Fix hydration error in layout

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbopack: {
-      root: process.cwd(),
-    },
-  },
   productionBrowserSourceMaps: false,
   async redirects() {
     return [
